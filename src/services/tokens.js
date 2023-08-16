8d6ad17a5de95ff17e5f6ba8acd355a3ee3f501e
beauty-joel <joel.cantu@solbeautyandcare.com>
2023-08-16 09:04:49 -0700

    Added transactions for new token and account

--- src/services/tokens.js
+++ src/services/tokens.js
@@ -84,26 +84,27 @@ exports.saveToken = async (requestBody) => {
   try {
     await db.runTransaction(async (transaction) => {
       let topics = [];
+
+      // Firestore document references
       const tokenDocument = await transaction.get(tokenReference);
       const accountDocument = await transaction.get(accountReference);
+
       if (tokenDocument.exists) {
         throw new Error("Token already exists!");
       }
 
       if (accountDocument.exists) {
         const { [sourceField]: tokens, ...theRest } = accountDocument.data();
-        console.log(tokens[0]);
         const singleTokenReference = db.doc(`tokenDetails/${tokens[0]}`);
         const singleTokenDocument = await transaction.get(singleTokenReference);
         topics = singleTokenDocument.data().topics;
 
         if (topics.length > 0) {
           console.log("Topic Subscription");
-          //   topics.forEach(async (topic) => {
-          //     await getMessaging().subscribeToTopic(token, topic);
-          //   });
+          topics.forEach(async (topic) => {
+            await getMessaging().subscribeToTopic(token, topic);
+          });
         }
-        console.log(topics);
 
         // Save token details
         const tokenData = {
@@ -121,20 +122,28 @@ exports.saveToken = async (requestBody) => {
           [sourceField]: FieldValue.arrayUnion(token),
         });
       } else {
+        const tokenData = {
+          createdAt: Timestamp.now(),
+          deviceType,
+          email,
+          topics: [],
+          source,
+        };
+
+        const newTokenReference = db.collection("tokenDetails").doc(token);
+        const newAccountReference = db.collection("deviceTokens").doc(email);
+
+        transaction.set(newTokenReference, tokenData);
+        transaction.set(newAccountReference, {
+          [sourceField]: [token],
+        });
       }
-
-      // Add one person to the city population.
-      // Note: this could be done without a transaction
-      //       by updating the population using FieldValue.increment()
-      //   const newPopulation = doc.data().population + 1;
-      //   t.update(cityRef, { population: newPopulation });
-      return {
-        status: "success",
-        message: "Token saved correctly",
-      };
     });
-
     console.log("Transaction success!");
+    return {
+      status: "success",
+      message: "Token saved correctly",
+    };
   } catch (error) {
     console.log("Transaction failure:", error);
     return {
